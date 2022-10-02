8f66a9d2799d00168803541c6e6b6691abfb0842
Winnie Xu <Winnie.xu97@gmail.com>
2022-10-02 11:14:28 -0400

    Update test_jax.py

--- test_jax.py
+++ test_jax.py
@@ -1,11 +1,11 @@
-import jax.numpy as np
+import jax.numpy as jnp
 from jax import random
 import jax
 
 key = jax.random.PRNGKey(0)
 x = jax.random.normal(key, (5000, 5000))
-print(np.dot(x, x.T) / 2)
-print(np.dot(x, x.T) / 2)
+print(jnp.dot(x, x.T) / 2)
+print(jnp.dot(x, x.T) / 2)
 
 """
 Use:

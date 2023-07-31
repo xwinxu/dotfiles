a005c4e864efd4c779287bd86767300b138f40db
Winnie Xu <winnie.xu97@gmail.com>
2023-07-31 20:58:07 +0000

    update settup to not clone this repo

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd $HOME
-git clone https://github.com/xwinxu/dotfiles.git && cp dotfiles/.bashrc .bashrc && cp dotfiles/.vimrc .vimrc && cp dotfiles/.tmux.conf .tmux.conf && source ~/.bashrc
+#git clone https://github.com/xwinxu/dotfiles.git
+cp ~/dotfiles/.bashrc .bashrc && cp ~/dotfiles/.vimrc .vimrc && cp ~/dotfiles/.tmux.conf .tmux.conf && source ~/.bashrc
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
